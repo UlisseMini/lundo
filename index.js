f988f969c9fef9ae96c0d484a71fc2cfe8d5035e
Ulisse Mini <ulisse.mini@gmail.com>
2021-11-16 15:27:27 -0500

    Switch to main.lua default

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const parser = require('luaparse')
 const fs = require('fs')
 const pretty = require('./pretty.js')
 
-var contents = fs.readFileSync('obtest.lua', {encoding: 'utf8'})
+var contents = fs.readFileSync('main.lua', {encoding: 'utf8'})
 var ast = parser.parse(contents, {encodingMode: 'pseudo-latin1'})
 
 
